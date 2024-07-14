eb515011cd538ced2f9b34dd93a5ae4c79bc68fc
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-14 19:32:12 -0300

    Add comment for function to create OpenSearch index if not exists

--- dev_environment/src/server/utils/create_index.ts
+++ dev_environment/src/server/utils/create_index.ts
@@ -1,6 +1,10 @@
 import { OpenSearchClient } from 'opensearch-dashboards/server';
 import { TODO_INDEX } from '../constants';
 
+/**
+ * The function `createIndexIfNotExists` checks if a specified index exists in an OpenSearch client and creates it if it
+ * does not.
+ */
 export async function createIndexIfNotExists(openSearchClient: OpenSearchClient) {
   if (!(await openSearchClient.indices.exists({ index: TODO_INDEX }))) {
     await openSearchClient.indices.create({ index: TODO_INDEX });

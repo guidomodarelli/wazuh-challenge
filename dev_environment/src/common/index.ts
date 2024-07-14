5d3cd8e2327dd5af2f840a1242cf35ae93790a69
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-14 12:35:46 -0300

    Add deleteIds route path in common index file

--- dev_environment/src/common/index.ts
+++ dev_environment/src/common/index.ts
@@ -3,3 +3,4 @@ export const PLUGIN_NAME = 'TO-DO plugin';
 export const SERVER_TODO_BASE_ROUTE_PATH = '/api/todo_plugin';
 export const SERVER_TODO_ROUTE_PATH_CREATE = `${SERVER_TODO_BASE_ROUTE_PATH}/create`;
 export const SERVER_TODO_ROUTE_PATH_GET_ALL = `${SERVER_TODO_BASE_ROUTE_PATH}/getAll`;
+export const SERVER_TODO_ROUTE_PATH_DELETE_IDS = `${SERVER_TODO_BASE_ROUTE_PATH}/deleteIds`;

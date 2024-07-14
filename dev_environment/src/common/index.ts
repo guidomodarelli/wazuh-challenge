dfe0f3cd45b7e98c6fb8ebd22b29cd4eacb36c5d
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-13 22:23:44 -0300

    Add new server routes for creating and getting all todos

--- dev_environment/src/common/index.ts
+++ dev_environment/src/common/index.ts
@@ -1,4 +1,5 @@
 export const PLUGIN_ID = 'todoPlugin';
 export const PLUGIN_NAME = 'TO-DO plugin';
 export const SERVER_TODO_BASE_ROUTE_PATH = '/api/todo_plugin';
-export const SERVER_TODO_ROUTE_PATH_EXAMPLE = `${SERVER_TODO_BASE_ROUTE_PATH}/example`;
+export const SERVER_TODO_ROUTE_PATH_CREATE = `${SERVER_TODO_BASE_ROUTE_PATH}/create`;
+export const SERVER_TODO_ROUTE_PATH_GET_ALL = `${SERVER_TODO_BASE_ROUTE_PATH}/getAll`;

62570e23e54653fec6dcd8286c429f3ee6c8edcd
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-13 22:47:09 -0300

    Add MainPage component with TodoTable to Main Page

---
+++ dev_environment/src/public/pages/MainPage.tsx
@@ -0,0 +1,34 @@
+import { EuiPage, EuiPageBody, EuiPageContent, EuiPageContentBody, EuiPageHeader, EuiTitle } from "@elastic/eui";
+import { FormattedMessage } from "@osd/i18n/react";
+import React from 'react';
+import { PLUGIN_NAME } from "../../common";
+import Todos from "../components/TodoTable/TodoTable";
+
+interface MainPageProps {}
+
+const MainPage = ({}: MainPageProps) => {
+  return (
+    <EuiPage restrictWidth="1280px">
+      <EuiPageBody component="main">
+        <EuiPageHeader>
+          <EuiTitle size="l">
+            <h1>
+              <FormattedMessage
+                id="todoPlugin.ToDoTitle"
+                defaultMessage="{name}"
+                values={{ name: PLUGIN_NAME }}
+              />
+            </h1>
+          </EuiTitle>
+        </EuiPageHeader>
+        <EuiPageContent>
+          <EuiPageContentBody>
+            <Todos />
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+  );
+};
+
+export default MainPage;

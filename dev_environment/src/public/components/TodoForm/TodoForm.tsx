1de3a58d82f8ddbe30462ff1883c457415b7867e
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-14 15:59:46 -0300

    Add new date fields and validation triggers

--- dev_environment/src/public/components/TodoForm/TodoForm.tsx
+++ dev_environment/src/public/components/TodoForm/TodoForm.tsx
@@ -43,20 +43,32 @@ const TodoForm = ({ id, update, onSuccess }: TodoFormProps) => {
       priority: [Priority.LOW],
       dueDate: undefined,
       plannedDate: undefined,
+      startedDate: undefined,
+      completedDate: undefined,
     },
   });
 
+  /**
+   * The function `renderStatusOptions` renders a `TodoStatus` component based on the label value of the provided
+   * `EuiComboBoxOptionOption` option.
+   * @param value - an object that represents an option in a combo box, where the `label` property is of type `Status`.
+   */
   const renderStatusOptions = (value: EuiComboBoxOptionOption<Option<typeof Status>>) => (
     <TodoStatus variant={value.label as Status} />
   );
 
+  /**
+   * The function `renderPriorityOptions` renders a `TodoPriority` component based on the selected value from a combo
+   * box.
+   * @param value - an object likely contains information about a priority option for a todo item.
+   */
   const renderPriorityOptions = (value: EuiComboBoxOptionOption<Option<typeof Priority>>) => (
     <TodoPriority variant={value.label as Priority} />
   );
 
   /**
-   * The function `changePlannedDateHandler` takes an `onChange` function and a `moment` parameter, converts the `moment`
-   * to a Date object, and triggers a 'dueDate' event.
+   * The function `changePlannedDateHandler` updates the value of the 'plannedDate' field and triggers validation for
+   * 'plannedDate' and 'dueDate'.
    */
   const changePlannedDateHandler = (moment?: Moment | null) => {
     setValue('plannedDate', moment?.toDate());
@@ -64,16 +76,49 @@ const TodoForm = ({ id, update, onSuccess }: TodoFormProps) => {
     trigger('dueDate');
   };
 
+  /**
+   * The `changeDueDateHandler` function sets the value of a form field to the selected date and triggers a validation
+   * check.
+   */
   const changeDueDateHandler = (moment?: Moment | null) => {
     setValue('dueDate', moment?.toDate());
     trigger('dueDate');
   };
 
+  /**
+   * The function `changeStartedDateHandler` updates the value of the 'startedDate' field and triggers validation for
+   * 'startedDate' and 'completedDate'.
+   */
+  const changeStartedDateHandler = (moment?: Moment | null) => {
+    setValue('startedDate', moment?.toDate());
+    trigger('startedDate');
+    trigger('completedDate');
+  };
+
+  /**
+   * The function `changeCompletedDateHandler` sets the value of a completed date field and triggers a validation check.
+   */
+  const changeCompletedDateHandler = (moment?: Moment | null) => {
+    setValue('completedDate', moment?.toDate());
+    trigger('completedDate');
+  };
+
+  /**
+   * The function `isInvalid` checks if there is an error message for a specific field in a set of `FieldValues`.
+   * @param {keyof FieldValues | 'root'} field - a key of `FieldValues` or the string `'root'`.
+   * @returns a boolean value indicating whether there is a message associated with the `field` in the `errors` object.
+   * If there is a message, it will return `true`, otherwise it will return `false`.
+   */
   const isInvalid = (field: keyof FieldValues | 'root') => {
     return !!errors[field]?.message;
   };
 
-  const submitHandler: SubmitHandler<FieldValues> = (data) => {
+  /**
+   * The submitHandler function creates a new todo item based on the form data, then calls the createTodo function,
+   * resets the form, and triggers the onSuccess callback if provided.
+   * @param data - It is being used to create a new todo item (`newTodo`).
+   */
+  const submitHandler: SubmitHandler<FieldValues> = (data: FieldValues) => {
     const newTodo: CreateTodoItem = {
       ...data,
       priority: data.priority[0],
@@ -195,6 +240,46 @@ const TodoForm = ({ id, update, onSuccess }: TodoFormProps) => {
                 onChange={changeDueDateHandler}
                 onClear={() => changeDueDateHandler()}
                 placeholder={'Select a due date'}
+              />
+            </EuiFormRow>
+          )}
+        />
+        <Controller
+          control={control}
+          name="startedDate"
+          render={({ field: { ref: _ref /* Not used */, value, onChange, ...otherFields } }) => (
+            <EuiFormRow
+              label="Started date"
+              isInvalid={isInvalid(otherFields.name)}
+              error={errors[otherFields.name]?.message}
+            >
+              <EuiDatePicker
+                {...otherFields}
+                isInvalid={isInvalid(otherFields.name)}
+                selected={moment(value)}
+                onChange={changeStartedDateHandler}
+                onClear={() => changeStartedDateHandler()}
+                placeholder={'Select a started date'}
+              />
+            </EuiFormRow>
+          )}
+        />
+        <Controller
+          control={control}
+          name="completedDate"
+          render={({ field: { ref: _ref /* Not used */, value, onChange, ...otherFields } }) => (
+            <EuiFormRow
+              label="Completed date"
+              isInvalid={isInvalid(otherFields.name)}
+              error={errors[otherFields.name]?.message}
+            >
+              <EuiDatePicker
+                {...otherFields}
+                isInvalid={isInvalid(otherFields.name)}
+                selected={moment(value)}
+                onChange={changeCompletedDateHandler}
+                onClear={() => changeCompletedDateHandler()}
+                placeholder={'Select a completed date'}
               />
             </EuiFormRow>
           )}

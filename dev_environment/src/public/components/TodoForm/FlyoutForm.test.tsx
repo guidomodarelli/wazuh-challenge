ef536d0ef4a56b99561bd371576fa140f0eb67e6
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-15 10:31:43 -0300

    Add unit tests for FlyoutForm component

---
+++ dev_environment/src/public/components/TodoForm/FlyoutForm.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import FlyoutForm from './FlyoutForm';
+import { I18nProvider } from '@osd/i18n/react';
+import { getByTestSubj } from '../../../test/utils';
+
+const onClose = jest.fn();
+jest.mock('./TodoForm', () => () => <></>);
+
+describe('FlyoutForm', () => {
+  it('should display a "Create new TODO" title', () => {
+    render(
+      <I18nProvider>
+        <FlyoutForm onClose={onClose} />
+      </I18nProvider>
+    );
+
+    expect(screen.getByText('Create new TODO')).toBeTruthy();
+    expect(screen.getByText('Create')).toBeTruthy();
+  });
+
+  it('should display a "Update TODO" title', () => {
+    render(
+      <I18nProvider>
+        <FlyoutForm onClose={onClose} itemIdToUpdate="ksdfsd" />
+      </I18nProvider>
+    );
+
+    expect(screen.getByText('Update TODO')).toBeTruthy();
+    expect(screen.getByText('Update')).toBeTruthy();
+  });
+
+  it('ensure `onClose` function is called', () => {
+    render(
+      <I18nProvider>
+        <FlyoutForm onClose={onClose} itemIdToUpdate="ksdfsd" />
+      </I18nProvider>
+    );
+
+    fireEvent.click(screen.getByText('Cancel'));
+    expect(onClose).toHaveBeenCalledTimes(1);
+    fireEvent.click(getByTestSubj('euiFlyoutCloseButton'));
+    expect(onClose).toHaveBeenCalledTimes(2);
+  });
+});

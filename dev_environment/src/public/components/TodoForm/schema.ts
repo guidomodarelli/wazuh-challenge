9703c6260a0fb431604ee4a33d64750e811f466d
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-14 22:27:12 -0300

    Update TodoForm schema with new field "isCompleted".

--- dev_environment/src/public/components/TodoForm/schema.ts
+++ dev_environment/src/public/components/TodoForm/schema.ts
@@ -1,61 +1,11 @@
 import { z } from 'zod';
-import { MIN_DATE_MESSAGE, MUST_BE_GREATER_THAN_OR_EQUAL_TO } from '../../constants';
-import moment from 'moment';
-import { Priority, Status } from "../../../common/types";
+import { Priority, Status } from '../../../common/types';
 
-export const schema = z
-  .object({
-    title: z.string().min(3),
-    status: z.enum(Object.values(Status) as [Status]).array(),
-    priority: z.enum(Object.values(Priority) as [Priority]).array(),
-    plannedDate: z
-      .date()
-      .min(new Date(), {
-        message: MIN_DATE_MESSAGE(new Date()),
-      })
-      .optional(),
-    dueDate: z
-      .date()
-      .min(new Date(), {
-        message: MIN_DATE_MESSAGE(new Date()),
-      })
-      .optional(),
-    startedDate: z
-      .date()
-      .min(new Date(), {
-        message: MIN_DATE_MESSAGE(new Date()),
-      })
-      .optional(),
-    completedDate: z
-      .date()
-      .min(new Date(), {
-        message: MIN_DATE_MESSAGE(new Date()),
-      })
-      .optional(),
-  })
-  .refine(
-    ({ dueDate, plannedDate }) => {
-      if (dueDate && plannedDate) {
-        return moment(dueDate).startOf('day') >= moment(plannedDate).startOf('day');
-      }
-      return true;
-    },
-    {
-      message: MUST_BE_GREATER_THAN_OR_EQUAL_TO('due date', 'planned date'),
-      path: ['dueDate'],
-    }
-  )
-  .refine(
-    ({ startedDate, completedDate }) => {
-      if (startedDate && completedDate) {
-        return moment(completedDate).startOf('day') >= moment(startedDate).startOf('day');
-      }
-      return true;
-    },
-    {
-      message: MUST_BE_GREATER_THAN_OR_EQUAL_TO('completed date', 'started date'),
-      path: ['completedDate'],
-    }
-  );
+export const schema = z.object({
+  title: z.string().min(3),
+  status: z.enum(Object.values(Status) as [Status]).array(),
+  priority: z.enum(Object.values(Priority) as [Priority]).array(),
+  isCompleted: z.boolean(),
+});
 
 export type FieldValues = z.infer<typeof schema>;

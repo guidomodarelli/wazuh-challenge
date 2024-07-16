28c6a4b15d69f05d49241b5c87f07d6a29be18f3
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-16 11:38:20 -0300

    Add RecordMock utility type for mocking objects

--- dev_environment/src/common/global-types.ts
+++ dev_environment/src/common/global-types.ts
@@ -1,4 +1,7 @@
 export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type RecordMock<T> = {
+  [K in keyof T]: T[K] extends Function ? jest.Mock : T[K] extends {} ? RecordMock<T[K]> : T[K];
+};
 export type PartialRecordMock<T> = Partial<
   {
     [K in keyof T]: T[K] extends Function

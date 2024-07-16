602dd947c36e43eb71a0830bd6c044127fd5334b
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-16 10:28:50 -0300

    Add comments explaining the purpose of functions and constants

--- dev_environment/src/public/components/TodoTable/useSelectedItems.hook.ts
+++ dev_environment/src/public/components/TodoTable/useSelectedItems.hook.ts
@@ -5,10 +5,16 @@ import { useState } from 'react';
 function useSelectedItems() {
   const [selectedItems, setSelectedItems] = useState<TodoItem['id'][]>([]);
 
+  /**
+   * The function `onSelectionChange` takes an array of `TodoItem` objects, extracts their `id` properties, and sets
+   * them as the selected items.
+   * @param {TodoItem[]} selectedItems - The `selectedItems` parameter is an array of `TodoItem['id']` strings.
+   */
   const onSelectionChange = (selectedItems: TodoItem[]) => {
     setSelectedItems(selectedItems.map((todoItem) => todoItem.id));
   };
 
+  /* The `selection` constant is defining an object of type `EuiTableSelectionType<TodoItem>`. */
   const selection: EuiTableSelectionType<TodoItem> = {
     selectableMessage: (selectable: boolean, todoItem: TodoItem) =>
       !selectable ? `${todoItem.title} is completed` : `Select ${todoItem.title}`,

22745d1b6e7aed6b1c8933a21a61b689192975f3
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-16 11:28:14 -0300

    Add PartialRecordMock type definition to global-types.ts

--- dev_environment/src/common/global-types.ts
+++ dev_environment/src/common/global-types.ts
@@ -1,1 +1,10 @@
 export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type PartialRecordMock<T> = Partial<
+  {
+    [K in keyof T]: T[K] extends Function
+      ? jest.Mock
+      : T[K] extends {}
+      ? PartialRecordMock<T[K]>
+      : T[K];
+  }
+>;

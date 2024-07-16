5732c225f37c9f3b8fba8f8c0add3ff9ad7db28a
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-15 22:09:34 -0300

    Add SERVER_TODO_ROUTE_PATH_BULK_CREATE constant

--- dev_environment/src/common/index.ts
+++ dev_environment/src/common/index.ts
@@ -2,5 +2,6 @@ export const PLUGIN_ID = 'todoPlugin';
 export const PLUGIN_NAME = 'TO-DO plugin';
 export const SERVER_TODO_BASE_ROUTE_PATH = '/api/todo_plugin';
 export const SERVER_TODO_ROUTE_PATH_CREATE = `${SERVER_TODO_BASE_ROUTE_PATH}/create`;
+export const SERVER_TODO_ROUTE_PATH_BULK_CREATE = `${SERVER_TODO_BASE_ROUTE_PATH}/bulkCreate`;
 export const SERVER_TODO_ROUTE_PATH_GET_ALL = `${SERVER_TODO_BASE_ROUTE_PATH}/getAll`;
 export const SERVER_TODO_ROUTE_PATH_DELETE_IDS = `${SERVER_TODO_BASE_ROUTE_PATH}/deleteIds`;

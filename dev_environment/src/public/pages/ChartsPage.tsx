ee2932f527bca549ff52f01938770f4cbdb816fa
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-16 10:50:14 -0300

    Add Layout component and refactor ChartsPage and MainPage

--- dev_environment/src/public/pages/ChartsPage.tsx
+++ dev_environment/src/public/pages/ChartsPage.tsx
@@ -1,33 +1,14 @@
-import { EuiPage, EuiPageBody, EuiPageContent, EuiPageContentBody, EuiPageHeader, EuiTitle } from "@elastic/eui";
-import { FormattedMessage } from '@osd/i18n/react';
 import React from 'react';
-import { PLUGIN_NAME } from "../../common";
-import Charts from "../components/Charts/Charts";
+import Charts from '../components/Charts/Charts';
+import Layout from '../layouts/Layout';
 
-interface ChartsProps {}
+interface ChartsPageProps {}
 
-const ChartsPage = ({}: ChartsProps) => {
+const ChartsPage = ({}: ChartsPageProps) => {
   return (
-    <EuiPage restrictWidth="1280px">
-      <EuiPageBody component="main">
-        <EuiPageHeader>
-          <EuiTitle size="l">
-            <h1>
-              <FormattedMessage
-                id="todoPlugin.ToDoTitle"
-                defaultMessage="{name}"
-                values={{ name: PLUGIN_NAME }}
-              />
-            </h1>
-          </EuiTitle>
-        </EuiPageHeader>
-        <EuiPageContent>
-          <EuiPageContentBody>
-            <Charts />
-          </EuiPageContentBody>
-        </EuiPageContent>
-      </EuiPageBody>
-    </EuiPage>
+    <Layout>
+      <Charts />
+    </Layout>
   );
 };
 
--- dev_environment/src/public/pages/MainPage.tsx
+++ dev_environment/src/public/pages/MainPage.tsx
@@ -1,40 +1,14 @@
-import {
-  EuiPage,
-  EuiPageBody,
-  EuiPageContent,
-  EuiPageContentBody,
-  EuiPageHeader,
-  EuiTitle,
-} from '@elastic/eui';
-import { FormattedMessage } from '@osd/i18n/react';
 import React from 'react';
-import { PLUGIN_NAME } from '../../common';
 import Todos from '../components/TodoTable/TodoTable';
+import Layout from '../layouts/Layout';
 
 interface MainPageProps {}
 
 const MainPage = ({}: MainPageProps) => {
   return (
-    <EuiPage restrictWidth="1280px">
-      <EuiPageBody component="main">
-        <EuiPageHeader>
-          <EuiTitle size="l">
-            <h1>
-              <FormattedMessage
-                id="todoPlugin.ToDoTitle"
-                defaultMessage="{name}"
-                values={{ name: PLUGIN_NAME }}
-              />
-            </h1>
-          </EuiTitle>
-        </EuiPageHeader>
-        <EuiPageContent>
-          <EuiPageContentBody>
-            <Todos />
-          </EuiPageContentBody>
-        </EuiPageContent>
-      </EuiPageBody>
-    </EuiPage>
+    <Layout>
+      <Todos />
+    </Layout>
   );
 };
 

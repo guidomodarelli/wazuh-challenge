42f66d871cf7384f245740b8640576d5444389e7
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-17 00:23:15 -0300

    Add test for displaying FlyoutForm when 'New' is clicked

---
+++ dev_environment/src/public/components/TodoTable/TodoTable.test.tsx
@@ -0,0 +1,23 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { mockTodoContextFn } from '../../../__mocks__/todo-context.mock';
+import * as TodoContext from '../../context/TodoContext';
+import TodoTable from './TodoTable';
+
+let mockTodoContext: ReturnType<typeof mockTodoContextFn>;
+
+describe('TodoTable', () => {
+  beforeEach(() => {
+    mockTodoContext = mockTodoContextFn(jest);
+
+    jest.spyOn(TodoContext, 'useTodoContext').mockImplementation(() => mockTodoContext);
+  });
+
+  it("verify that clicking 'New' button displays FlyoutForm", () => {
+    render(<TodoTable />);
+
+    fireEvent.click(screen.getByText('New'));
+
+    expect(screen.getByText('Create new TODO')).toBeTruthy();
+  });
+});

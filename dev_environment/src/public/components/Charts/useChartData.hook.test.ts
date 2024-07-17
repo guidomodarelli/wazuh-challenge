d771490929eb21b01d4e3862ed43bff22611b82a
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-17 01:51:37 -0300

    Add unit test for useChartData hook grouping by assignee

---
+++ dev_environment/src/public/components/Charts/useChartData.hook.test.ts
@@ -0,0 +1,42 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { mockTodoContextFn } from '../../../__mocks__/todo-context.mock';
+import { TodoItem } from '../../../common/types';
+import * as TodoContext from '../../context/TodoContext';
+import useChartData from './useChartData.hook';
+
+let mockTodoContext: ReturnType<typeof mockTodoContextFn>;
+
+describe('useChartData', () => {
+  beforeEach(() => {
+    mockTodoContext = mockTodoContextFn(jest);
+    jest.spyOn(TodoContext, 'useTodoContext').mockImplementation(() => mockTodoContext);
+  });
+
+  it('should ', () => {
+    const todoItems: Pick<TodoItem, 'assignee'>[] = [
+      {
+        assignee: 'Tyler',
+      },
+      {
+        assignee: 'Jadyn',
+      },
+      {
+        assignee: 'Nicholas',
+      },
+      {
+        assignee: 'Nicholas',
+      },
+    ];
+    const { result } = renderHook(() => useChartData());
+
+    // @ts-expect-error
+    const response = result.current.groupByAssignee(todoItems[3], todoItems);
+
+    expect(response).toBe(2);
+
+    // @ts-expect-error
+    const response = result.current.groupByAssignee(todoItems[1], todoItems);
+
+    expect(response).toBe(1);
+  });
+});

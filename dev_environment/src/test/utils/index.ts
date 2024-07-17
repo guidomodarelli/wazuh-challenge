c28e3d923074b551f631cb2b9f1b481e7e1b092b
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-17 14:59:05 -0300

    Add getByTestSubj function for selecting elements by id

--- dev_environment/src/test/utils/index.ts
+++ dev_environment/src/test/utils/index.ts
@@ -1,5 +1,11 @@
 import { screen } from '@testing-library/react';
 
+/**
+ * The function getByTestSubj returns an element based on the data-test-subj attribute matching the provided id.
+ * @param {string} id - a string that represents the value of the `data-test-subj` attribute that you want to select.
+ * @returns an element that has a `data-test-subj` attribute with a value equal to the `id` parameter passed to the
+ * function.
+ */
 export const getByTestSubj = (id: string) => {
   return screen.getByLabelText((content, element) => {
     return element?.getAttribute('data-test-subj') === id;

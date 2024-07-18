9fab9ec6140a66244aeb8098df75f62de2637b28
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-17 21:10:37 -0300

    Add Priority and Status enums; create TodoEntity class

---
+++ dev_environment/src/public/core/domain/entities/Status.ts
@@ -0,0 +1,6 @@
+export enum Status {
+  NOT_STARTED = 'Not started',
+  IN_PROGRESS = 'Working on it',
+  COMPLETED = 'Completed',
+  EXECUTED_WITH_ERROR = 'Error',
+}
---
+++ dev_environment/src/public/core/domain/entities/TodoEntity.ts
@@ -0,0 +1,27 @@
+import { v4 as UUID } from 'uuid';
+import { OmitStrict } from '../../../../common/types';
+import { Priority } from './Priority';
+import { Status } from './Status';
+
+export type TodoEntityId = string;
+export type TodoEntityRequest = OmitStrict<TodoEntity, 'id' | 'createdAt'>;
+
+export class TodoEntity {
+  id: TodoEntityId;
+  title: string;
+  status: Status;
+  priority: Priority;
+  createdAt: string;
+  tags?: string[] | undefined;
+  assignee?: string | undefined;
+
+  constructor(data: TodoEntityRequest) {
+    this.id = UUID();
+    this.title = data.title;
+    this.status = data.status;
+    this.priority = data.priority;
+    this.createdAt = new Date().toISOString();
+    this.tags = data.tags ?? [];
+    this.assignee = data.assignee;
+  }
+}

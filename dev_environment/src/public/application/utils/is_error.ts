30d668368f23001c1c2ea0a304a2b0ecbec64e8f
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-18 02:09:59 -0300

    Remove isError utility function

--- dev_environment/src/public/application/utils/is_error.ts
+++
@@ -1,9 +0,0 @@
-import { HttpFetchError } from '../../../../../src/core/public';
-
-/**
- * The function `isError` checks if the input is an instance of `HttpFetchError`.
- * @returns a boolean value indicating whether the `error` parameter is an instance of `HttpFetchError`.
- */
-export function isError<T>(error: T | HttpFetchError): error is HttpFetchError {
-  return error instanceof HttpFetchError;
-}
